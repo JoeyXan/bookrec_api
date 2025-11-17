da28edfedd84935d4a59090bf87596d979f97b6a
JoeyXan <joseph.escalante.est@tecazuay.edu.ec>
2025-11-16 21:11:23 -0500

    modified:   bookrec_streamlit.py

--- bookrec_streamlit.py
+++ bookrec_streamlit.py
@@ -16,8 +16,12 @@ mode = st.sidebar.radio("Modo de uso", ["Usar API FastAPI", "Cargar artefactos l
 k = st.sidebar.slider("Cantidad de recomendaciones (k)", 1, 20, 5)
 
 if mode == "Usar API FastAPI":
-   base_url = st.sidebar.text_input("URL de la API", value="http://localhost:8000")
-    #base_url = st.sidebar.text_input("URL de la API", value="http://localhost:8000")
+    # Intenta leer de secrets, si no existe usa localhost
+    try:
+        default_api = st.secrets["API_URL"]
+    except:
+        default_api = os.getenv("API_URL", "http://localhost:8000")
+    base_url = st.sidebar.text_input("URL de la API", value=default_api)        
 else:
     model_dir = st.sidebar.text_input("Carpeta de modelos", value="models")
     data_dir = st.sidebar.text_input("Carpeta de datos (para historial de usuario)", value="data")
